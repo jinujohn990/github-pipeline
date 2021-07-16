b42073c53ef8efe35e471b2f4ec31dfaac27c605
jinujohn990 <jinujohn990@gmail.com>
2021-07-16 20:35:18 +0530

    pom commit

--- SpringBootTesting/src/main/java/com/jinu/dom/SpringBootTestingApplication.java
+++ SpringBootTesting/src/main/java/com/jinu/dom/SpringBootTestingApplication.java
@@ -2,17 +2,26 @@ package com.jinu.dom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-@EnableScheduling
+//@EnableAsync
+//@EnableScheduling
+//@EnableFeignClients
+@EnableZuulProxy
 public class SpringBootTestingApplication {
 
 	public static void main(String[] args) {
+		try {
 		SpringApplication.run(SpringBootTestingApplication.class, args);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	
 	}
-
 	
 }
--- SpringBootTesting/src/main/java/com/jinu/dom/controller/TestController.java
+++ SpringBootTesting/src/main/java/com/jinu/dom/controller/TestController.java
@@ -1,18 +1,25 @@
 package com.jinu.dom.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.jinu.dom.client.UserClient;
+import com.jinu.dom.dto.UserPost;
 import com.jinu.dom.service.TestSercvice;
-
+@Lazy
 @Controller
 public class TestController {
 	@Autowired
 	private TestSercvice testSercvice;
+	@Autowired
+	UserClient userClient;
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(HttpServletRequest request) {
@@ -32,5 +39,10 @@ public class TestController {
 		System.out.println("In springbootdevtools method...");
 		System.out.println("After springbootdevtools service.....");
 		return "something";
+	}
+	@RequestMapping(value = "/userPost", method = RequestMethod.GET)
+	public String getUserPost() {
+		 List<UserPost> userpostlist = userClient.getUserPostlist();
+		 return userpostlist.toString();
 	}
 }
